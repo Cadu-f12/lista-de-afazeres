e08e0d0fd286eca055803274bbaf133ad976384f
CarlosWindows <cadudailha68@gmail.com>
2026-02-09 15:49:00 -0300

    feat: Display at website the tasks

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -1,10 +1,22 @@
 const todoList = [];
 
 function addToList() {
-    const inputName = document.querySelector('.js-input-name')
+    const inputElement = document.querySelector('.js-input-name');
+    todoList.push(inputElement.value);
 
-    todoList.push(inputName.value);
-    console.log(todoList);
+    renderHTML();
 
-    inputName.value = '';
+    inputElement.value = '';
+}
+function renderHTML() {
+    const paragraphElement = document
+        .querySelector('.js-paragraph-output');
+    
+    let text = '';
+
+    for (i = 0; i < todoList.length; i++) {
+        text += `<p>${todoList[i]}</p>`;
+    }
+
+    paragraphElement.innerHTML = text;
 }
